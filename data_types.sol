1dab8246d292abc7036a457eba91cf38fd695c1a
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2024-03-03 00:40:36 +0530

    data-typeo.sol

--- data_types.sol
+++ data_types.sol
@@ -2,5 +2,30 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    constructor() {}
+    // state variables
+    uint public myUint = 1;
+    //uint256, uint8, int256
+
+    string public str = "Hello, world!";
+    bytes32 public myBytes32 = "Hello, world!";
+
+    address public myAddress;
+
+    struct MyStruct {
+        uint256 myUint256;
+        string myString;
+    }
+
+    MyStruct public myStruct = MyStruct(1, "Hello, world!");
+
+    constructor() {
+        myAddress = msg.sender;
+    }
+
+    // Local Variables
+    function getValue() public pure returns (uint) {
+        uint value = 0;
+        value = value + 1;
+        return value;
+    }
 }
